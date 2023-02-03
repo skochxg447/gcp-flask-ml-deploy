57c93a637938a30a4a8a73052e77467ec16b83fe
Grant Koch <123999256+skochxg447@users.noreply.github.com>
2023-02-02 17:53:34 -0800

    testing delivery again after adjusting trigger

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! Booya yalllll'
+    return 'Hello World! Booya yalll'
 
 @app.route('/echo/<name>')
 def echo(name):
